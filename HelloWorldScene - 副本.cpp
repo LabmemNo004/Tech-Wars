af5d604b24d2399dd13527918b26cca08599b1b0
G root <43871477+Dw108@users.noreply.github.com>
2019-04-30 13:33:32 +0800

    Add files via upload

---
+++ HelloWorldScene - 副本.cpp
@@ -0,0 +1,145 @@
+#include "HelloWorldScene.h"
+#include"Inputfieldbox.h"
+#include<string>
+#include"Ball/BallTest.h"
+USING_NS_CC;
+using namespace CocosDenshion;
+
+Scene* HelloWorld::createScene()
+{
+    return HelloWorld::create();
+}
+
+// Print useful error message instead of segfaulting when files are not there.
+static void problemLoading(const char* filename)
+{
+    printf("Error while loading: %s\n", filename);
+    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Scene::init() )
+    {
+        return false;
+    }
+
+    auto visible_size = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	//create background layer
+	Sprite *bg = Sprite::create("menu/background_start.jpg");
+	bg->setPosition(Vec2(origin.x + visible_size.width / 2,
+		origin.y + visible_size.height / 2));
+	this->addChild(bg);
+	
+	//create start button
+	Sprite *startSpriteNormal = Sprite::create("menu/start01.png");
+	Sprite *startSpriteSelected = Sprite::create("menu/start02.png");
+
+	MenuItemSprite *startMenuItem = MenuItemSprite::create(startSpriteNormal,
+		startSpriteSelected,
+		CC_CALLBACK_1(HelloWorld::getGameScene, this));
+
+	startMenuItem->setPosition(Director::getInstance()->convertToGL(Vec2(400, 550)));
+
+	//create setting button
+	MenuItemImage *settingMenuItem = MenuItemImage::create(
+		"menu/setting01.png",
+		"menu/setting02.png",
+		CC_CALLBACK_1(HelloWorld::getSettingScene, this));
+
+	settingMenuItem->setPosition(Director::getInstance()->convertToGL(Vec2(640, 550)));
+
+	//create quit button
+	MenuItemImage *quitMenuItem = MenuItemImage::create(
+		"menu/quit01.png",
+		"menu/quit02.png",
+		CC_CALLBACK_1(HelloWorld::quitGame, this));
+
+	quitMenuItem->setPosition(Director::getInstance()->convertToGL(Vec2(880, 550)));
+
+    //create button layer
+	Menu* mu = Menu::create(startMenuItem, settingMenuItem, quitMenuItem, NULL);
+	mu->setPosition(Vec2::ZERO);
+	this->addChild(mu);
+
+    return true;
+}
+
+void HelloWorld::getGameScene(cocos2d::Object *pSender)
+{
+	auto sc = Inputfield::createScene();
+	auto reScene = TransitionShrinkGrow::create(0.7f, sc);
+	Director::getInstance()->pushScene(reScene);
+	if (UserDefault::getInstance()->getBoolForKey(SOUND_KEY)) {
+		SimpleAudioEngine::getInstance()->playEffect("sound/click.wav");
+	}
+}
+
+void HelloWorld::getSettingScene(cocos2d::Object *pSender)
+{
+	auto sc = Setting::createScene();
+	auto reScene = TransitionShrinkGrow::create(0.7f, sc);
+	Director::getInstance()->pushScene(reScene);
+	if (UserDefault::getInstance()->getBoolForKey(SOUND_KEY)) {
+		SimpleAudioEngine::getInstance()->playEffect("sound/click.wav");
+	}
+}
+
+void HelloWorld::quitGame(Object *pSender)
+{
+	//Close the cocos2d-x game scene and quit the application
+
+	Director::getInstance()->end();
+	if (UserDefault::getInstance()->getBoolForKey(SOUND_KEY)) {
+		SimpleAudioEngine::getInstance()->playEffect("sound/click.wav");
+	}
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+	exit(0);
+#endif
+
+	/*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
+
+	//EventCustom customEndEvent("game_scene_close_event");
+	//_eventDispatcher->dispatchEvent(&customEndEvent);
+
+}
+
+
+void HelloWorld::onEnter()
+{
+	Scene::onEnter();
+	log("HelloWorld onEnter");
+}
+//开始播放
+void HelloWorld::onEnterTransitionDidFinish()
+{
+	Scene::onEnterTransitionDidFinish();
+	log("HelloWorld onEnterTransitionDidFinish");
+
+	if (UserDefault::getInstance()->getBoolForKey(MUSIC_KEY)) {
+		SimpleAudioEngine::getInstance()->playBackgroundMusic("sound/music.mp3", true);
+	}
+}
+void HelloWorld::onExit()
+{
+	Scene::onExit();
+	log("HelloWorld onExit");
+}
+
+void HelloWorld::onExitTransitionDidStart()
+{
+	Scene::onExitTransitionDidStart();
+	log("HelloWorld onExitTransitionDidStart");
+}
+//停止播放
+void HelloWorld::cleanup()
+{
+	Scene::cleanup();
+	log("HelloWorld cleanup");
+	SimpleAudioEngine::getInstance()->stopBackgroundMusic("sound/music.mp3");
+}
---
+++ SettingScene1.cpp
@@ -0,0 +1,181 @@
+#include "SettingScene1.h"
+
+USING_NS_CC;
+using namespace CocosDenshion;
+
+Scene* Setting::createScene()
+{
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
+
+	// 'layer' is an autorelease object
+	auto layer = Setting::create();
+
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
+}
+
+// on "init" you need to initialize your instance
+bool Setting::init()
+{
+	//////////////////////////////
+	// 1. super init first
+	if (!Layer::init())
+	{
+		return false;
+	}
+	log("Setting init");
+
+
+	Size visible_size = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	Sprite *bg = Sprite::create("menu/background_setting.jpg");
+
+	// position the label on the center of the screen
+	bg->setPosition(Vec2(origin.x + visible_size.width / 2,
+		origin.y + visible_size.height / 2));
+	this->addChild(bg);
+
+	//ÒôÐ§
+	auto soundOnMenuItem = MenuItemImage::create(
+		"menu/on.png",
+		"menu/on.png");
+	auto soundOffMenuItem = MenuItemImage::create(
+		"menu/off.png",
+		"menu/off.png");
+
+	auto soundToggleMenuItem = MenuItemToggle::createWithCallback(CC_CALLBACK_1(Setting::menuSoundToggleCallback, this),
+		soundOnMenuItem,
+		soundOffMenuItem,
+		NULL);
+	soundToggleMenuItem->setPosition(Director::getInstance()->convertToGL(Vec2(800, 200)));
+
+	//ÒôÀÖ
+	auto musicOnMenuItem = MenuItemImage::create(
+		"menu/on.png",
+		"menu/on.png");
+	auto musicOffMenuItem = MenuItemImage::create(
+		"menu/off.png",
+		"menu/off.png");
+	auto musicToggleMenuItem = MenuItemToggle::createWithCallback(CC_CALLBACK_1(Setting::menuMusicToggleCallback, this),
+		musicOnMenuItem,
+		musicOffMenuItem,
+		NULL);
+	musicToggleMenuItem->setPosition(Director::getInstance()->convertToGL(Vec2(800, 340)));
+
+	//Ok°´Å¥
+	auto okMenuItem = MenuItemImage::create(
+		"menu/ok-down.png",
+		"menu/ok-up.png",
+		CC_CALLBACK_1(Setting::menuOkCallback, this));
+
+	okMenuItem->setPosition(Director::getInstance()->convertToGL(Vec2(600, 510)));
+
+	Menu* mn = Menu::create(soundToggleMenuItem, musicToggleMenuItem, okMenuItem, NULL);
+	mn->setPosition(Vec2::ZERO);
+	this->addChild(mn);
+
+	UserDefault *defaults = UserDefault::getInstance();
+
+	if (defaults->getBoolForKey(MUSIC_KEY)) {
+		musicToggleMenuItem->setSelectedIndex(0);//off
+	}
+	else {
+		musicToggleMenuItem->setSelectedIndex(1);//on
+	}
+
+	if (defaults->getBoolForKey(SOUND_KEY)) {
+		soundToggleMenuItem->setSelectedIndex(0);//off
+	}
+	else {
+		soundToggleMenuItem->setSelectedIndex(1);//on
+	}
+
+
+	return true;
+}
+
+void Setting::menuOkCallback(Ref* pSender)
+{
+	Director::getInstance()->popScene();
+
+	if (UserDefault::getInstance()->getBoolForKey(SOUND_KEY)) {
+		SimpleAudioEngine::getInstance()->playEffect("sound/click.wav");
+	}
+}
+
+void Setting::menuSoundToggleCallback(Ref* pSender)
+{
+	auto soundToggleMenuItem = (MenuItemToggle*)pSender;
+	log("soundToggleMenuItem %d", soundToggleMenuItem->getSelectedIndex());
+
+	UserDefault *defaults = UserDefault::getInstance();
+	if (defaults->getBoolForKey(SOUND_KEY)) {
+		defaults->setBoolForKey(SOUND_KEY, false);
+	}
+	else {
+		defaults->setBoolForKey(SOUND_KEY, true);
+		SimpleAudioEngine::getInstance()->playEffect("sound/click.wav");
+	}
+
+}
+
+
+void Setting::menuMusicToggleCallback(Ref* pSender)
+{
+	auto musicToggleMenuItem = (MenuItemToggle*)pSender;
+	log("musicToggleMenuItem %d", musicToggleMenuItem->getSelectedIndex());
+
+	UserDefault *defaults = UserDefault::getInstance();
+	if (defaults->getBoolForKey(MUSIC_KEY)) {
+		defaults->setBoolForKey(MUSIC_KEY, false);
+		SimpleAudioEngine::getInstance()->stopBackgroundMusic();
+	}
+	else {
+		defaults->setBoolForKey(MUSIC_KEY, true);
+		SimpleAudioEngine::getInstance()->playBackgroundMusic("sound/music.mp3");
+	}
+}
+
+
+void Setting::onEnter()
+{
+	Layer::onEnter();
+	log("Setting onEnter");
+}
+
+void Setting::onEnterTransitionDidFinish()
+{
+	Layer::onEnterTransitionDidFinish();
+	log("Setting onEnterTransitionDidFinish");
+
+	if (UserDefault::getInstance()->getBoolForKey(MUSIC_KEY)) {
+		//²¥·Å
+		SimpleAudioEngine::getInstance()->playBackgroundMusic("sound/music.mp3", true);
+	}
+
+}
+
+void Setting::onExit()
+{
+	Layer::onExit();
+	log("Setting onExit");
+}
+
+void Setting::onExitTransitionDidStart()
+{
+	Layer::onExitTransitionDidStart();
+	log("Setting onExitTransitionDidStart");
+}
+
+void Setting::cleanup()
+{
+	Layer::cleanup();
+	log("Setting cleanup");
+	//Í£Ö¹
+	SimpleAudioEngine::getInstance()->stopBackgroundMusic("sound/music.mp3");
+}
