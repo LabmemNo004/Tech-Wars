463c682a2b75fdd0fd06ef4b4fe9ab2b0ac2134a
LabmemNo004 <1085201315@qq.com>
2019-06-03 23:42:56 +0800

    Online(ver1.0)
    实现了联网和单机模式的独立运行
    实现角色选择和各角色基本攻击方法
    待完善：血条绑定。升级技能。退出联机
    联机人物选择。代码编写

--- Thirdtry/Source/Thirdtry/Thirdtry.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "Thirdtry.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Thirdtry, "Thirdtry" );
-
-DEFINE_LOG_CATEGORY(LogThirdtry)
- 
